73599ea0d5c490ea6c09d58672ec684186771ad5
abao <abao@qq.com>
2021-01-13 21:22:40 +0800

    "提交gittest类"

---
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author mangguodong
+ * @create 2021-01-13
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello idea git");
+    }
+}
