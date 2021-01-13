b4d7dc3fb56ecf90a98d9d9637dd5fc0c6fabb06
abao <abao@qq.com>
2021-01-13 21:28:18 +0800

    "hot分支提交1"

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -9,5 +9,9 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello idea git");
         System.out.println("hello branch");
+        System.out.println("hello branch");
+        System.out.println("hello branch");
+        System.out.println("hello branch");
+        System.out.println("hello branch");
     }
 }
