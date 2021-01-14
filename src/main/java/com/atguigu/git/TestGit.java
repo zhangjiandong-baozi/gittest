48398a158467f5ad0dd48166ead2f7f8ef6484ce
abao <abao@qq.com>
2021-01-14 10:43:54 +0800

    "主分支提交2"

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -9,5 +9,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello idea git");
         System.out.println("hello branch");
+        System.out.println("主分支上传了代码");
     }
 }
